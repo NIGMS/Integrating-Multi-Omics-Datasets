47c6e63bdc921b1617e0921aa5ea5870f9d8451d
hadiparsianNIH <hadi.parsian@nih.gov>
2025-03-04 15:45:55 -0500

    added bash installation for submodule 3

---
+++ AWS/03-Integration/install_integration_packages.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Create a conda environment
+conda create -n r-package-3 r-base=4.3.3 -y
+
+# Consider addressing your conda initialization instead
+source ~/.bashrc
+
+# Activate the Conda Environment
+conda activate r-package-3
+
+# Install packages
+
+conda install conda-forge::r-pvclust -y
+
+conda install conda-forge::r-ggnewscale -y 
+
+conda install conda-forge::r-ggridges -y
+
+conda install conda-forge::r-europepmc -y
+
+conda install conda-forge::r-ggseqlogo -y
+
+conda install bioconda::bioconductor-methreg -y
+
+conda install bioconda::bioconductor-bsgenome -y
+
+conda install bioconda::bioconductor-clusterprofiler -y
+
+conda install bioconda::bioconductor-pathview -y
+
+conda install bioconda::bioconductor-enrichplot -y
+
+conda install bioconda::bioconductor-repitools -y
+
+conda install bioconda::bioconductor-rnaagecalc -y 
+
+conda install bioconda::bioconductor-motifmatchr -y
+
+conda install bioconda::bioconductor-tfbstools -y 
+
+
+
+R -e 'install.packages(c("IRkernel"), repos="http://cran.rstudio.com/")'
+
+# Install the kernel specification for Jupyter
+R -e 'IRkernel::installspec(name = "R-Intergration", displayname = "R-Intergration")' 
